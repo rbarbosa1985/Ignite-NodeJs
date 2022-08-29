d9a76c5045a45e8336d6e6121865fefe6430a000
Roberto dos Santos Barbosa <betoanselmo@gmail.com>
2022-08-29 20:31:00 -0300

    Count

---
+++ Conta/src/index.js
@@ -0,0 +1,16 @@
+const express = require(express);
+const app = express();
+
+/*
+* cpf - string
+* name - string
+* id - uuid
+* statement []
+*/
+
+app.post("/account", (request, response) => {
+  const {cpf, name} = request.body;
+  
+})
+
+app.listen(3333);
